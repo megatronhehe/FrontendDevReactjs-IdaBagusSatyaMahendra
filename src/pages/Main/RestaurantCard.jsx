330df4668b39a1c8ac2a4d66d349f8eda382c2c4
Megatronhehee <satyamahendra09@gmail.com>
2023-10-31 22:38:03 +0700

    fix: better restaurant status indicator

--- src/pages/Main/RestaurantCard.jsx
+++ src/pages/Main/RestaurantCard.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 
-import { TbStarFilled, TbStarHalfFilled } from "react-icons/tb";
+import { TbStarFilled, TbStarHalfFilled, TbCircleFilled } from "react-icons/tb";
 
 export default function RestaurantCard({ restaurant }) {
 	const { name, photo, cuisine, price_level, is_closed } = restaurant;
 
-	return (
-		<li className="flex flex-col gap-2 w-full">
-			<img
-				src={photo.images.original.url}
-				alt={photo.caption}
-				className="h-40 object-cover"
+	const restaurantStatus = (
+		<li className="text-gray-500 flex items-center gap-1">
+			<TbCircleFilled
+				className={`${is_closed ? "text-red-400" : "text-green-400"}`}
 			/>
+			{is_closed ? "CLOSED" : "OPEN NOW"}
+		</li>
+	);
+
+	return (
+		<li className="flex flex-col justify-around gap-2 w-full group h-80">
+			<div className="h-1/2 w-full overflow-hidden">
+				<img
+					src={photo.images.original.url}
+					alt={photo.caption}
+					className="h-full w-full object-cover group-hover:scale-125 duration-200"
+				/>
+			</div>
 
 			<h2 className="font-normal">{name}</h2>
 
@@ -27,10 +38,10 @@ export default function RestaurantCard({ restaurant }) {
 				<li className="text-gray-400">
 					{cuisine[0].name} - {price_level}
 				</li>
-				<li className="text-gray-500">{is_closed ? "CLOSED" : "OPEN NOW"}</li>
+				{restaurantStatus}
 			</ul>
 
-			<button className="bg-blue-950 text-white text-sm py-2">
+			<button className="bg-blue-950 border border-blue-950 hover:bg-white text-white text-sm py-2 hover:text-blue-950 duration-200">
 				LEARN MORE
 			</button>
 		</li>
